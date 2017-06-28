bf673f57ddb4f125af2605c0902c06b96fbaf269
LynnMac <buaa.zyh@gmail.com>
2017-06-28 09:12:56 +0800

    redirect 1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SEPS' });
 });
+
+router.get('/index.html',function (req,res,next) {
+    res.render('index',{title:'SEPS'});
+
+});
+router.get('/src-teacher.html',function (req,res,next) {
+    res.render('src-teacher',{title:'SEPS'})
+})
+
 
 module.exports = router;
