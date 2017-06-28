8acc85f457262018505c56afea94d2f2b9a1f704
LynnMac <buaa.zyh@gmail.com>
2017-06-28 14:24:50 +0800

    public

--- public/js/index.js
+++ public/js/index.js
@@ -28,26 +28,31 @@ $(function () {
         $('#dagang-btn').css('display','inline-block');
 
     });
-    $('.side-bar ').click(function () {
-        var _this=this;
-        var id=_this.id;
-
-        switch (id){
-            case 'courses':
-                var content=$.get('tc-cs.html');
-                $('.right-content').html(content);
-        }
-    });
+    // $('.side-bar ').click(function () {
+    //     var _this=this;
+    //     var id=_this.id;
+    //
+    //     switch (id){
+    //         case 'courses':
+    //             var content=$.get('tc-cs.html');
+    //             $('.right-content').html(content);
+    //     }
+    // });
     
     //course-teacher
     $('.collapsible').collapsible();
     
+    
+    
+    //src-tc
     $('.choosesrc').click(function () {
         $("[name='checkbox']").each(function(){
             if(!$(this).attr("checked")){
                 $(this).prop("checked",'true');
             }
-        })
+        });
+        // alert($('#src1').is(':checked'));
+
     });
     $('.choosenot').click(function () {
 
@@ -56,6 +61,21 @@ $(function () {
                     return !attr;
                 })
             })
+    });
+    
+    $('.src-public').click(function () {
+        var header=$('#srcheadline').val();
+        var content=$('#srccontent').val();
+        if(header&&content){
+            var headerhtml='<li> <div class="collapsible-header"> <div class="col s1 m1 selectDown"> <form> <p> <input type="checkbox" name="checkbox" class="filled-in" id="src1"> <label for="src1"></label> </p> </form> </div> <i class="material-icons">filter_drama</i>';
+            var middlehtml='<i class="material-icons text-black right">play_for_work</i> </div> <div class="collapsible-body"> <span>';
+            var footerhtml='</span> </div> </li>';
+
+            $('.show-src').append(headerhtml+header+middlehtml+content+footerhtml);
+
+        } else{
+            alert('请完整输入标题和文本说明');
+        }
     });
 
     $('.modal').modal();
@@ -67,7 +87,7 @@ $(function () {
         var _this=this;
         var li=$(_this).parent().parent();
         $('.show-tm').append(li);
-        $(_this).prev().remove();
+        $(_this).next().remove();
         $(_this).remove();
 
     });
@@ -76,7 +96,8 @@ $(function () {
         var _this=this;
         var li=$(_this).parent().parent();
         $(li).css('display','none');
-    })
+    });
+
 
     
     
