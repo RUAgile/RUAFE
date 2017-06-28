1ac6c225c212bf1295b9abdb675efd0df761b030
Kevin_Feng <Kevin_Feng@139.com>
2017-06-28 16:10:50 +0800

    完成学生团队界面

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,18 @@ router.get('/homework-stu.html', function(req, res, next) {
     res.render('homework-stu', { title: '作业' });
 });
 router.get('/resource-stu.html', function(req, res, next) {
-    res.render('resource-stu', { title: '作业' });
+    res.render('resource-stu', { title: '资源' });
 });
 router.get('/team-stu.html', function(req, res, next) {
-    res.render('team-stu', { title: '作业' });
+    res.render('team-stu', { title: '团队' });
 });
 router.get('/test.html', function(req, res, next) {
-    res.render('test', { title: '作业' });
+    res.render('test', { title: '测试' });
+});
+router.get('/im-stu.html', function(req, res, next) {
+    res.render('im-stu', { title: '交流' });
+});
+router.get('/classinfo-stu.html', function(req, res, next) {
+    res.render('classinfo-stu', { title: '课程信息' });
 });
 module.exports = router;
