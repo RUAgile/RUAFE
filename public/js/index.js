50c79a7508e37f688ca311d31f359860f389dbcc
LynnMac <buaa.zyh@gmail.com>
2017-06-28 16:17:26 +0800

    28-2-2

--- public/js/index.js
+++ public/js/index.js
@@ -3,29 +3,29 @@
  */
 $(function () {
     $('.side-bar').click(function () {
-        var _this=this;
+        var _this = this;
         $('.side-bar').removeClass('baractive');
         $(_this).addClass('baractive');
     });
-    
+
     $('#dagang-btn').click(function () {
-        var _this=this;
-        $('.edit-dagang').css('display','block');
-        $('.show-dagang').css('display','none');
-        $(_this).css('display','none');
+        var _this = this;
+        $('.edit-dagang').css('display', 'block');
+        $('.show-dagang').css('display', 'none');
+        $(_this).css('display', 'none');
 
     });
     $('.edit-cancel').click(function () {
-        $('.edit-dagang').css('display','none');
-        $('.show-dagang').css('display','block');
-        $('#dagang-btn').css('display','inline-block');
+        $('.edit-dagang').css('display', 'none');
+        $('.show-dagang').css('display', 'block');
+        $('#dagang-btn').css('display', 'inline-block');
     });
     $('.edit-submit').click(function () {
-        var dagang=$('.input-dagang').val();
-        $('.show-dagang').text('教学大纲: '+dagang);
-        $('.edit-dagang').css('display','none');
-        $('.show-dagang').css('display','block');
-        $('#dagang-btn').css('display','inline-block');
+        var dagang = $('.input-dagang').val();
+        $('.show-dagang').text('教学大纲: ' + dagang);
+        $('.edit-dagang').css('display', 'none');
+        $('.show-dagang').css('display', 'block');
+        $('#dagang-btn').css('display', 'inline-block');
 
     });
     // $('.side-bar ').click(function () {
@@ -38,17 +38,16 @@ $(function () {
     //             $('.right-content').html(content);
     //     }
     // });
-    
+
     //course-teacher
     $('.collapsible').collapsible();
-    
-    
-    
+
+
     //src-tc
     $('.choosesrc').click(function () {
-        $("[name='checkbox']").each(function(){
-            if(!$(this).attr("checked")){
-                $(this).prop("checked",'true');
+        $("[name='checkbox']").each(function () {
+            if (!$(this).attr("checked")) {
+                $(this).prop("checked", 'true');
             }
         });
         // alert($('#src1').is(':checked'));
@@ -56,39 +55,53 @@ $(function () {
     });
     $('.choosenot').click(function () {
 
-            $("[name='checkbox']").each(function(){//反选
-                $(this).prop('checked',function (index,attr) {
-                    return !attr;
-                })
+        $("[name='checkbox']").each(function () {//反选
+            $(this).prop('checked', function (index, attr) {
+                return !attr;
             })
+        })
     });
-    
+    $('.src-del').click(function () {
+        $("[name='checkbox']").each(function () {
+            if($(this).prop('checked')){
+                $(this).parents('li').css('display','none');
+            }
+        });
+    });
+
     $('.src-public').click(function () {
-        var header=$('#srcheadline').val();
-        var content=$('#srccontent').val();
-        if(header&&content){
-            var headerhtml='<li> <div class="collapsible-header"> <div class="col s1 m1 selectDown"> <form> <p> <input type="checkbox" name="checkbox" class="filled-in" id="src1"> <label for="src1"></label> </p> </form> </div> <i class="material-icons">filter_drama</i>';
-            var middlehtml='<i class="material-icons text-black right">play_for_work</i> </div> <div class="collapsible-body"> <span>';
-            var footerhtml='</span> </div> </li>';
+        var header = $('#srcheadline').val();
+        var content = $('#srccontent').val();
+        if (header && content) {
+            var headerhtml = '<li> <div class="collapsible-header"> <div class="col s1 m1 selectDown"> <form> <p> <input type="checkbox" name="checkbox" class="filled-in" id="src1"> <label for="src1"></label> </p> </form> </div> <i class="material-icons">filter_drama</i>';
+            var middlehtml = '<i class="material-icons text-black right">play_for_work</i> </div> <div class="collapsible-body"> <span>';
+            var footerhtml = '</span> </div> </li>';
 
-            $('.show-src').append(headerhtml+header+middlehtml+content+footerhtml);
+            $('.show-src').append(headerhtml + header + middlehtml + content + footerhtml);
 
-        } else{
+        } else {
             Materialize.toast('请输入完整的标题和说明文字', 4000);
         }
     });
+    // tc-hw
+    $('.hw-del').click(function () {
+        $("[name='checkbox']").each(function () {
+            if($(this).prop('checked')){
+                $(this).parents('li').css('display','none');
+            }
+        });
+    });
+    $('.hw-public').on('click', function () {
+        var header = $('#hwheadline').val();
+        var content = $('#hwcontent').val();
+        if (header && content) {
+            var headerhtml = '<li> <div class="collapsible-header"> <div class="col s1 m1 selectDown"> <form> <p> <input type="checkbox" name="checkbox" class="filled-in" id="src1"> <label for="src1"></label> </p> </form> </div> <i class="material-icons">filter_drama</i>';
+            var middlehtml = '<i class="material-icons text-black right">play_for_work</i> </div> <div class="collapsible-body"> <span>';
+            var footerhtml = '</span> </div> </li>';
 
-    $('.hw-public').on('click',function () {
-        var header=$('#hwheadline').val();
-        var content=$('#hwcontent').val();
-        if(header&&content){
-            var headerhtml='<li> <div class="collapsible-header"> <div class="col s1 m1 selectDown"> <form> <p> <input type="checkbox" name="checkbox" class="filled-in" id="src1"> <label for="src1"></label> </p> </form> </div> <i class="material-icons">filter_drama</i>';
-            var middlehtml='<i class="material-icons text-black right">play_for_work</i> </div> <div class="collapsible-body"> <span>';
-            var footerhtml='</span> </div> </li>';
+            $('.show-tw').append(headerhtml + header + middlehtml + content + footerhtml);
 
-            $('.show-tw').append(headerhtml+header+middlehtml+content+footerhtml);
-
-        } else{
+        } else {
             Materialize.toast('请输入完整的标题和说明文字', 4000);
         }
     });
@@ -99,11 +112,11 @@ $(function () {
     //tc-tm.html
 
     $('.app-pass').click(function () {
-        var _this=this;
-        var newbutton='<a class="waves-effect waves-light btn tm-edit">调整团队</a> <a class="waves-effect waves-light btn red tm-del">解散团队</a>';
+        var _this = this;
+        var newbutton = '<a class="waves-effect waves-light btn tm-edit">调整团队</a> <a class="waves-effect waves-light btn red tm-del">解散团队</a>';
 
-        var li=$(_this).parent().parent();
-        var div=$(_this).parent();
+        var li = $(_this).parent().parent();
+        var div = $(_this).parent();
         $(div).append(newbutton);
 
         $('.show-tm').append(li);
@@ -111,32 +124,28 @@ $(function () {
         $(_this).remove();
 
 
-
     });
 
     $('.app-reject').click(function () {
-        var _this=this;
-        var li=$(_this).parent().parent();
-        $(li).css('display','none');
+        var _this = this;
+        var li = $(_this).parent().parent();
+        $(li).css('display', 'none');
     });
-    
-    $('.tm-del').on('click',function () {
-        var _this=this;
-        var li=$(_this).parent().parent();
-        $(li).css('display','none');
+
+    $('.tm-del').on('click', function () {
+        var _this = this;
+        var li = $(_this).parent().parent();
+        $(li).css('display', 'none');
     });
-    $('.collapsible-body').on('click','.tm-del',function () {
-        var _this=this;
-        var li=$(_this).parent().parent();
-        $(li).css('display','none');
+    $('.collapsible-body').on('click', '.tm-del', function () {
+        var _this = this;
+        var li = $(_this).parent().parent();
+        $(li).css('display', 'none');
     });
-    
+
     $('.tm-edit').click(function () {
-
+        //TODO
     });
 
 
-
-    
-    
 })
